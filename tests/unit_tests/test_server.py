8dd619cc5717ba7c1c237bcfea0359d9cf150cc1
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-02-06 09:31:26 +0100

    unit test login OK

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -1,3 +1,16 @@
+from server import clubs, competitions
+
+
 def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_login_with_registered_email(client):
+    club = clubs[0]
+    data = {"email": club["email"]}
+    response = client.post("/show_summary", data=data, follow_redirects=True)
+    assert response.status_code == 200
+    assert bytes(club["points"], "utf8") in response.data
+    for competition in competitions:
+        assert bytes(competition["name"], "utf8") in response.data
