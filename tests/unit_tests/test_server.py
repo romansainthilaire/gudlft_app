963b8ee2edec3b1ffc9ef5a67367db939c5ebe54
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-02-08 07:22:44 +0100

    refactoring

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -20,7 +20,7 @@ def test_login_with_registered_email(client):
 
 
 def test_login_with_unregistered_email(client):
-    data = {"email": "test@gmail.com"}
+    data = {"email": "email.doesnotexist@gmail.com"}
     response = client.post("/show_summary", data=data, follow_redirects=True)
     assert response.status_code == 200
     assert b"email-not-found-message" in response.data
