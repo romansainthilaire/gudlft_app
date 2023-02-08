629f27f7bf473713d49c95a0dc4ed23d25d6c998
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-02-08 17:22:54 +0100

    initial

--- tests/functional_tests/test_server.py
+++ tests/functional_tests/test_server.py
@@ -12,7 +12,7 @@ def test_login_with_unregistered_email(page):
     page.get_by_label("Email:").fill("email.doesnotexist@gmail.com")
     page.get_by_role("button", name="Enter").click()
     assert page.title() == "Registration | GUDLFT"
-    assert page.query_selector("#email-not-found-message") is not None
+    assert page.query_selector("li").inner_text() == "Sorry, that email wasn't found."
 
 
 def test_login_clubs_list_logout(page):
@@ -39,6 +39,32 @@ def test_login_clubs_list_logout(page):
     assert page.title() == "Registration | GUDLFT"
 
 
+def test_login_book_15_places(page):
+    page.goto(BASE_URL)
+    purchased_places = 15
+
+    # login
+    page.get_by_label("Email:").click()
+    page.get_by_label("Email:").fill(club["email"])
+    page.get_by_role("button", name="Enter").click()
+
+    # book places
+    (page
+     .get_by_role("paragraph")
+     .filter(has_text=(
+         f"{competition['name']} " +
+         f"Date: {competition['date']} " +
+         f"Places left: {competition['numberOfPlaces']} " +
+         "Book Places"))
+     .get_by_role("link", name="Book Places")
+     .click())
+    page.get_by_label("How many places?").click()
+    page.get_by_label("How many places?").fill(str(purchased_places))
+    page.get_by_role("button", name="Book").click()
+    assert page.title() == f"Booking for {competition['name']} | GUDLFT"
+    assert page.query_selector("li").inner_text() == "You can book 12 places max per competition."
+
+
 def test_login_book_10_places(page):
     page.goto(BASE_URL)
     purchased_places = 10
@@ -49,7 +75,8 @@ def test_login_book_10_places(page):
     page.get_by_role("button", name="Enter").click()
 
     # book places
-    (page.get_by_role("paragraph")
+    (page
+     .get_by_role("paragraph")
      .filter(has_text=(
          f"{competition['name']} " +
          f"Date: {competition['date']} " +
@@ -61,4 +88,6 @@ def test_login_book_10_places(page):
     page.get_by_label("How many places?").fill(str(purchased_places))
     page.get_by_role("button", name="Book").click()
     assert page.title() == "Summary | GUDLFT"
-    assert page.query_selector("#points-available").inner_text() == f"Points available: {club['points'] - 10}"
+    assert (page
+            .query_selector("#points-available")
+            .inner_text() == f"Points available: {club['points'] - purchased_places}")
